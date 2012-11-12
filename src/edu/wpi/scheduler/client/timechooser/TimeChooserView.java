4db57d4f2ae0d121241795406883dd551aca36e6
unknown <ryandanas@gmail.com>
2012-11-11 21:00:48 -0500

    Started working on choose time table

--- src/edu/wpi/scheduler/client/timechooser/TimeChooserView.java
+++ src/edu/wpi/scheduler/client/timechooser/TimeChooserView.java
@@ -4,7 +4,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.wpi.scheduler.client.controller.StudentSchedule;
@@ -16,8 +19,12 @@ public class TimeChooserView extends Composite {
 
 	interface TimeChooserViewUiBinder extends UiBinder<Widget, TimeChooserView> {
 	}
-
+	
+	@UiField
+	Grid timeGrid;
+	
 	public TimeChooserView(StudentSchedule studentSchedule) {
+		
 		initWidget(uiBinder.createAndBindUi(this));
 		
 		getElement().getStyle().setLeft(0, Unit.PX);
@@ -25,7 +32,15 @@ public class TimeChooserView extends Composite {
 		getElement().getStyle().setTop(0, Unit.PX);
 		getElement().getStyle().setBottom(0, Unit.PX);
 		getElement().getStyle().setPosition(Position.ABSOLUTE);
-
+		
+		int row = timeGrid.getRowCount();
+		int col = timeGrid.getColumnCount();
+		
+		for (int i = 0; i < row; i++){
+			for (int j = 0; j < col; j++){
+				timeGrid.setWidget(i, j, new Label(i +"," + j));
+			}
+		}
 	}
 
 }
